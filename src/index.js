4df05109a4b00154e3005a880198ba1e30b3a9ca
SzponarPatryk <szponar.patryk@gmail.com>
2019-08-30 00:16:40 +0200

    Adding general styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Homepage from './pages/HomePage'
 import './assets/styles/reset.css'
+import './assets/styles/general.css'
+
+import Homepage from './pages/HomePage'
 
 import { unregister as unregisterServiceWorker } from './utils/serviceWorker'
 
